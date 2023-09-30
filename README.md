f017ecef2a87c88be425e3971a7fbeb11078bebe
Lorans_Alkhateeb <lwrnsalkhtyb9@gmail.com>
2023-09-30 08:21:33 -0700

    done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # caffe_z
+# This is the first task of java script contain the if conditions in java script 
+## if statment and if else in java script and switch case statment
---
+++ cafffe_z.js
@@ -0,0 +1,30 @@
+
+const name = prompt("Please enter your name:");
+
+
+const gender = prompt("Please enter your gender (male or female):").toLowerCase();
+
+
+if (gender === "male" || gender === "female") {
+    const title = gender === "male" ? "Mr" : "Ms";
+    console.log('Welcome, ${title}.,${name}!');
+
+ 
+    const drinkType = prompt("Do you want a hot or cold drink? (hot/cold):");
+
+    const drinkName = prompt("Please enter the name of your drink:");
+
+    console.log(`Your ${drinkType} drink, ${drinkName}, is getting prepared.`);
+
+    console.log('${name} ordered a ${drinkType} drink: ${drinkName}');
+} else {
+    console.log('Welcome, ${name}!');
+
+    const drinkType = prompt("Do you want a hot or cold drink? (hot/cold):");
+
+    const drinkName = prompt("Please enter the name of your drink:");
+
+    console.log('Your ${drinkType} drink, ${drinkName}, is getting prepared.');
+
+    console.log('${name} ordered a ${drinkType} drink: ${drinkName}');
+}
